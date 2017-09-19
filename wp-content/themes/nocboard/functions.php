4561ddffbd0eb0ecd077f183649f8f3f09012e5d
MatiRosero <gekidasa@gmail.com>
2017-09-18 23:04:39 -0600

    first commit

---
+++ wp-content/themes/nocboard/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'pinboard_parent_theme_enqueue_styles' );
+
+function pinboard_parent_theme_enqueue_styles() {
+    wp_enqueue_style( 'pinboard-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'nocboard-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'pinboard-style' )
+    );
+
+}
