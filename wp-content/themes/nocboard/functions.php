883360a73052af6af68360fd101e04854ce9b21b
MatiRosero <gekidasa@gmail.com>
2018-02-12 23:31:09 -0600

    include parent theme in sass

--- wp-content/themes/nocboard/functions.php
+++ wp-content/themes/nocboard/functions.php
@@ -3,10 +3,9 @@
 add_action( 'wp_enqueue_scripts', 'pinboard_parent_theme_enqueue_styles' );
 
 function pinboard_parent_theme_enqueue_styles() {
-    wp_enqueue_style( 'pinboard-style', get_template_directory_uri() . '/style.css' );
+    // wp_enqueue_style( 'pinboard-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'nocboard-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( 'pinboard-style' )
+        get_stylesheet_directory_uri() . '/assets/dist/style.css'
     );
 
 }
